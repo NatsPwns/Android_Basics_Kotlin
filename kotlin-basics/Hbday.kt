9e93151e125d79a8493dbbaa202c15c39fb96fbf
Nat <natisantiago@yandex.ru>
2022-09-17 15:41:24 -0500

    Happy Birthday card built in Kotlin

---
+++ kotlin-basics/Hbday.kt
@@ -0,0 +1,22 @@
+fun main() {
+    val age = 7
+    val name = "Tommie"
+    
+    println("=+=+=+=+=+=+=+=+=+=+=+=+")
+    println("Happy Birthday, ${name}!")
+    println("=+=+=+=+=+=+=+=+=+=+=+=+")
+   
+    // Let's print a cake!
+    println("   ,,,,,   ")
+    println("   |||||   ")
+    println(" =========")
+    println("@@@@@@@@@@@")
+    println("{~@~@~@~@~}")
+    println("@@@@@@@@@@@")
+    
+    // This prints an empty line.
+    println("")
+
+    println("You are already ${age} years old, ${name}!")
+    println("${age} is the very best age to celebrate!")
+}
